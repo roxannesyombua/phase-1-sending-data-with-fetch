70c6495c39099bae2ccc722f5a3784b3d9f88b0e
roxannesyombua <roxannesyombua@gmail.com>
2024-04-12 07:49:53 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(name, email) {
+    let userData ={
+        name: name,
+        email: email
+    }
+    return fetch('http://localhost:3000/users', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify(userData)
+          
+}).then(response => response.json())
+.then(data => {
+    
+        const div = document.createElement('div');
+        div.innerHTML =  `ID: ${data.id}`
+        document.body.appendChild(div);
+
+    
+    console.log(data)
+    return data
+    
+})
+.catch(err => {
+    const errlog = document.createElement('p');
+    errlog.innerHTML = "Unauthorized Access"
+    document.body.appendChild(errlog);
+    return err
+})
+
+}
